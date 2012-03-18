ae77392d0cb2dbb0dee7c2aa23586cba066ea6fc
Rob Hoelz <rob@hoelz.ro>
2012-03-18 14:27:21 +0100

    Implement the module

--- lib/Dist/Zilla/Plugin/Git/ExcludeUntracked.pm
+++ lib/Dist/Zilla/Plugin/Git/ExcludeUntracked.pm
@@ -2,8 +2,37 @@
 package Dist::Zilla::Plugin::Git::ExcludeUntracked;
 
 ## use critic (RequireUseStrict)
-use strict;
-use warnings;
+use Moose;
+
+with 'Dist::Zilla::Role::FilePruner';
+
+
+sub _assemble_untracked_lookup {
+    my ( $self ) = @_;
+
+    my @untracked_files = map {
+        chomp; $_
+    } qx(git ls-files --other);
+
+    return map { $_ => 1 } @untracked_files;
+}
+
+sub prune_files {
+    my ( $self ) = @_;
+
+    my $zilla            = $self->zilla;
+    my @files            = @{ $zilla->files };
+    my %untracked_lookup = $self->_assemble_untracked_lookup;
+
+    foreach my $file (@files) {
+        if(exists $untracked_lookup{$file->name}) {
+            $self->log_debug([ 'pruning %s', $file->name ]);
+            $zilla->prune_file($file);
+        }
+    }
+}
+
+__PACKAGE__->meta->make_immutable;
 
 1;
 
