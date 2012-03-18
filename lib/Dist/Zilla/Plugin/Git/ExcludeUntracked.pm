dcfd498af91b3537c001cf6e6536f33a7fd893e2
Rob Hoelz <rob@hoelz.ro>
2012-03-18 13:29:43 +0100

    initial commit

---
+++ lib/Dist/Zilla/Plugin/Git/ExcludeUntracked.pm
@@ -0,0 +1,22 @@
+## no critic (RequireUseStrict)
+package Dist::Zilla::Plugin::Git::ExcludeUntracked;
+
+## use critic (RequireUseStrict)
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+# ABSTRACT:  A short description of Dist::Zilla::Plugin::Git::ExcludeUntracked
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=head1 SEE ALSO
+
+=cut
